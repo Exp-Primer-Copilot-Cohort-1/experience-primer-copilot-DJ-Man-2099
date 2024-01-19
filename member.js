56c94319c7ee044609b7af84cb126dd8480ddfbd
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2024-01-19 13:55:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsmember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Python', 'Django', 'Flask']
+    var member = {
+        name: 'Nguyen Van A',
+        age: 20,
+        address: 'Ha Noi',
+        skills: skills
+    }
+    console.log(member)
+    console.log(member.name)
+    console.log(member.age)
+    console.log(member.address)
+    console.log(member.skills)
+    console.log(member.skills[1])
+    console.log(member.skills[2])
+    console.log(member.skills[3])
+    console.log(member.skills[4])
+    console.log(member.skills[5])
+    console.log(member.skills[6])
+}
